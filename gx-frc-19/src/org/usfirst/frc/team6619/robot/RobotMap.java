70eaa55679482b7687127018cd7ff5e9af5500b3
MaxThePrisberry <31422721+MaxThePrisberry@users.noreply.github.com>
2018-10-15 16:40:38 -0700

    Add talon import
    I added the basic talon import to the RobotMap and made sure it worked with libraries.

--- gx-frc-19/src/org/usfirst/frc/team6619/robot/RobotMap.java
+++ gx-frc-19/src/org/usfirst/frc/team6619/robot/RobotMap.java
@@ -7,6 +7,7 @@
 
 package org.usfirst.frc.team6619.robot;
 
+import com.ctre.phoenix.motorcontrol.can.*;
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -14,6 +15,7 @@ package org.usfirst.frc.team6619.robot;
  * floating around.
  */
 public class RobotMap {
+	TalonSRX talon = new TalonSRX(0);
 	// For example to map the left and right motors, you could define the
 	// following variables to use with your drivetrain subsystem.
 	// public static int leftMotor = 1;
