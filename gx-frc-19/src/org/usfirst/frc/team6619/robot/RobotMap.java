3151b358cf9d3078e0d4f035f68c2cbd88a7cc8d
MaxThePrisberry <31422721+MaxThePrisberry@users.noreply.github.com>
2018-11-02 19:37:30 -0700

    General Import and Talon
    I have changed the classpath to include the generic phoenix libraries that will come naturally on any regularly installed installers. I also added the import line of the talon libraries into RobotMap.java.

--- gx-frc-19/src/org/usfirst/frc/team6619/robot/RobotMap.java
+++ gx-frc-19/src/org/usfirst/frc/team6619/robot/RobotMap.java
@@ -7,6 +7,7 @@
 
 package org.usfirst.frc.team6619.robot;
 
+import com.ctre.phoenix.motorcontrol.can.*;
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
