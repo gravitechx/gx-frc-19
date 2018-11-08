4bc04039142b4e91480c53ecbf6aa45f92477833
MaxThePrisberry <31422721+MaxThePrisberry@users.noreply.github.com>
2018-11-08 08:18:08 -0800

    Update limit turnary
    Fixed the limit function. Added parenthesis to prevent bugs. I also missed a 1 that is now changed to limit. See drive merge #2

--- gx-frc-19/src/org/gravitechx/frc2019/utils/driveutilities/DriveSignalUtilities.java
+++ gx-frc-19/src/org/gravitechx/frc2019/utils/driveutilities/DriveSignalUtilities.java
@@ -3,7 +3,7 @@ package org.gravitechx.frc2019.utils.driveutilities;
 public class DriveSignalUtilities {
 	//limit the signal to the specified limit. If above limit, set to limit. If below negative limit, set to negative limit. Else keep the same.
 	public double limit(double signal, double limit) {
-		signal = signal > limit ? 1 : signal < -limit ? -limit : signal;
+		signal = (signal > limit) ? limit : ((signal < -limit) ? -limit : signal);
 		return signal;
 	}
 	
