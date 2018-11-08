7b52840ed80e7e298f317c6ccd758a5f51b52c50
MaxThePrisberry <31422721+MaxThePrisberry@users.noreply.github.com>
2018-11-07 17:38:43 -0800

    Make the basic control scheme
    I added the joysticks to the RealControlScheme class, and added getters to get the values for the throttle and the wheel rotation.

--- gx-frc-19/src/org/gravitechx/frc2019/robot/io/controlschemes/RealControlScheme.java
+++ gx-frc-19/src/org/gravitechx/frc2019/robot/io/controlschemes/RealControlScheme.java
@@ -1,5 +1,24 @@
 package org.gravitechx.frc2019.robot.io.controlschemes;
 
-public class RealControlScheme {
+import edu.wpi.first.wpilibj.Joystick;
 
+public class RealControlScheme {
+	private static Joystick throttleJoystick;
+	private static Joystick rotationJoystick;
+	
+	//Contructors
+	protected RealControlScheme() {
+		throttleJoystick = new Joystick(0);//CHANGE TO BE REAL VALUES
+		rotationJoystick = new Joystick(1);//CHANGE TO BE REAL VALUES
+	}
+	
+	//Get the value of the acceleration joystick of the driver
+	public double getThrottle() {
+		return throttleJoystick.getY();
+	}
+	
+	//get the wheel rotation value of the driver
+	public double getRotation() {
+		return rotationJoystick.getY();
+	}
 }
