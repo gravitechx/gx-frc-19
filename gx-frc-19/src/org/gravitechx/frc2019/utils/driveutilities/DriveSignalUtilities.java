ef6d261b48cf9537f1872e65df29586bfb13ca56
MaxThePrisberry <31422721+MaxThePrisberry@users.noreply.github.com>
2018-11-08 08:28:34 -0800

    Changed to static
    Updated both methods to be static methods.

--- gx-frc-19/src/org/gravitechx/frc2019/utils/driveutilities/DriveSignalUtilities.java
+++ gx-frc-19/src/org/gravitechx/frc2019/utils/driveutilities/DriveSignalUtilities.java
@@ -2,13 +2,13 @@ package org.gravitechx.frc2019.utils.driveutilities;
 
 public class DriveSignalUtilities {
 	//limit the signal to the specified limit. If above limit, set to limit. If below negative limit, set to negative limit. Else keep the same.
-	public double limit(double signal, double limit) {
+	public static double limit(double signal, double limit) {
 		signal = (signal > limit) ? limit : ((signal < -limit) ? -limit : signal);
 		return signal;
 	}
 	
 	//if only given one value, assume it is the thing to limit. Pass it to above limit function with the limit of one.
-	public double limit(double signal) {
+	public static double limit(double signal) {
 		return limit(signal, 1.0);
 	}
 }
