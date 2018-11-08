f890c1a97653622dbdf389e6f5c5e1bcc4e52d37
MaxThePrisberry <31422721+MaxThePrisberry@users.noreply.github.com>
2018-11-07 17:50:34 -0800

    Made Constructor Class
    I made a Constants class in the robot package, and added constants for the throttle and wheel ports. I changed the controlscheme to use the constant values I made.

---
+++ gx-frc-19/src/org/gravitechx/frc2019/robot/Constants.java
@@ -0,0 +1,6 @@
+package org.gravitechx.frc2019.robot;
+
+public class Constants {
+	public static final int THROTTLE_JOYSTICK_PORT = 0;
+	public static final int ROTATION_JOYSTICK_PORT= 0;
+}
--- gx-frc-19/src/org/gravitechx/frc2019/robot/io/controlschemes/RealControlScheme.java
+++ gx-frc-19/src/org/gravitechx/frc2019/robot/io/controlschemes/RealControlScheme.java
@@ -1,15 +1,16 @@
 package org.gravitechx.frc2019.robot.io.controlschemes;
 
 import edu.wpi.first.wpilibj.Joystick;
+import org.gravitechx.frc2019.robot.Constants;
 
 public class RealControlScheme {
 	private static Joystick throttleJoystick;
 	private static Joystick rotationJoystick;
 	
-	//Contructors
+	//Constructors
 	protected RealControlScheme() {
-		throttleJoystick = new Joystick(0);//CHANGE TO BE REAL VALUES
-		rotationJoystick = new Joystick(1);//CHANGE TO BE REAL VALUES
+		throttleJoystick = new Joystick(Constants.THROTTLE_JOYSTICK_PORT);//Set the throttle joystick to a port specified in constants
+		rotationJoystick = new Joystick(Constants.ROTATION_JOYSTICK_PORT);//Set the wheel joystick to a port specified in constants
 	}
 	
 	//Get the value of the acceleration joystick of the driver
