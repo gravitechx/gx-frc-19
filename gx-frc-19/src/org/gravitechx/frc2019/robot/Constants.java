c25a16883e38d1c31533da3fb117bc7c2a3f8a3f
MaxThePrisberry <31422721+MaxThePrisberry@users.noreply.github.com>
2018-11-08 16:05:39 -0800

    Update Motorcontroller port values
    Changed all motorcontroller values at this point, adhering to the ports used in gx-frc-19.

--- gx-frc-19/src/org/gravitechx/frc2019/robot/Constants.java
+++ gx-frc-19/src/org/gravitechx/frc2019/robot/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
 	public static final int ROTATION_JOYSTICK_PORT= 0;
 	
 	//NEED TO GET THE TRUE VALUES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	public static final int LEFT_MASTER_TALON_PORT = 0;
-	public static final int RIGHT_MASTER_TALON_PORT = 0;
-	public static final int LEFT_SLAVE_VICTOR_PORT = 0;
-	public static final int RIGHT_SLAVE_VICTOR_PORT = 0;
+	public static final int LEFT_MASTER_TALON_PORT = 3;
+	public static final int RIGHT_MASTER_TALON_PORT = 1;
+	public static final int LEFT_SLAVE_VICTOR_PORT = 1;
+	public static final int RIGHT_SLAVE_VICTOR_PORT = 3;
 }
