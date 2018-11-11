3147164f3569e398752c8ecdd288f01b03569d0d
MaxThePrisberry <31422721+MaxThePrisberry@users.noreply.github.com>
2018-11-10 18:56:30 -0800

    Add limits
    The speeds are now limited to the constant we define in the Constants.java class.

--- gx-frc-19/src/org/gravitechx/frc2019/utils/driveutilities/RotationalDriveSignal.java
+++ gx-frc-19/src/org/gravitechx/frc2019/utils/driveutilities/RotationalDriveSignal.java
@@ -1,5 +1,7 @@
 package org.gravitechx.frc2019.utils.driveutilities;
 
+import org.gravitechx.frc2019.robot.Constants;
+
 public class RotationalDriveSignal {
 	private double speed, rotation;
 	
@@ -17,15 +19,15 @@ public class RotationalDriveSignal {
 	}
 	
 	public void limitValues() {
-		speed = DriveSignalUtilities.limit(speed);
-		rotation = DriveSignalUtilities.limit(rotation);
+		speed = DriveSignalUtilities.limit(speed, Constants.SPEED_LIMIT);
+		rotation = DriveSignalUtilities.limit(rotation, Constants.SPEED_LIMIT);
 	}
 	
 	public DifferentialDriveSignal toDifferentialDriveSignal() {
 		double giveLeft = speed + rotation;
 		double giveRight = speed - rotation;
-		DriveSignalUtilities.limit(giveLeft);
-		DriveSignalUtilities.limit(giveRight);
+		DriveSignalUtilities.limit(giveLeft, Contants.SPEED_LIMIT);
+		DriveSignalUtilities.limit(giveRight, Constants.SPEED_LIMIT);
 		return new DifferentialDriveSignal(giveLeft, giveRight);
 	}
 }
