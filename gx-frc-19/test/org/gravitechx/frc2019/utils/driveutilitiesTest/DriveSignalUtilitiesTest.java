873d24aff99229cf3687ad90c1c7ce5460cfff73
Nathan <nprisbrey@users.noreply.github.com>
2019-01-15 16:10:29 -0800

    Updated Comments in DriveSignalUtilitiesTest.java
    Updated comments in "DriveSignalUtilitiesTest.java".

--- gx-frc-19/test/org/gravitechx/frc2019/utils/driveutilitiesTest/DriveSignalUtilitiesTest.java
+++ gx-frc-19/test/org/gravitechx/frc2019/utils/driveutilitiesTest/DriveSignalUtilitiesTest.java
@@ -25,13 +25,11 @@ class DriveSignalUtilitiesTest {
 	
 	@ParameterizedTest(name = "{0} is scaled by {1} to a result of {2}")
 	@CsvSource({
-		// *long sigh*
-		// mitochondria is the powerhouse of the cell
+		// Why don't you just gradually change the boolean? :D
 		"0.5, 0.5, 0.25",
 		"1.2, 1.5, 1.8",
 		"-2, 2, -4",
 		"0.11, 2, 0.22"
-		// "I chose to sat here" --Nira 2018
 	})
 	void testScale(double signal, double scale, double result) {
 		assertEquals(result, DriveSignalUtilities.scale(signal, scale), 
