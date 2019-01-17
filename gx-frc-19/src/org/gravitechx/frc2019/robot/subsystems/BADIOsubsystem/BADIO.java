2121b05ec64da8c5adc1ac3b3d1375ef8efb777d
Nira <nira.nerdy@gmail.com>
2019-01-16 16:50:48 -0800

    Reorganization of BADIO subsystem

--- gx-frc-19/src/org/gravitechx/frc2019/robot/subsystems/BADIOsubsystem/BADIO.java
+++ gx-frc-19/src/org/gravitechx/frc2019/robot/subsystems/BADIOsubsystem/BADIO.java
@@ -4,7 +4,10 @@ import org.gravitechx.frc2019.robot.Constants;
 
 public class BADIO {
 	
-	private RANDOM_MOTOR_CONTROLLER /*fix later please*/ position;
+	private RANDOM_MOTOR_CONTROLLER /*fix later please*/ positionMotor;
+
+	private WPI_TalonSRX leftBIO;
+	private WPI_TalonSRX rightBIO;
 	
 	public enum positionOptions {
 		ONE, TWO, THREE, FOUR
@@ -15,36 +18,57 @@ public class BADIO {
 	private static final BADIO badio = new BADIO();
 	
 	private BADIO() {
-		position = new RANDOM_MOTOR_CONTROLLER /*FIX LATER*/(Constants.POSITION_MOTOR_PORT);
+		positionMotor = new RANDOM_MOTOR_CONTROLLER /*FIX LATER*/(Constants.POSITION_MOTOR_PORT);
 		positionStatus = positionOptions.ONE;
+
+		leftBIO = new WPI_TalonSRX(Constants.LEFT_BIO_TALON_PORT);
+		rightBIO = new WPI_TALONSRX(Constants.RIGHT_BIO_TALON_PORT);
 	}
 	
 	public void setPosition(positionOptions position) {
 		if (positionStatus != position) {
+			positionStatus = position;
 			switch (position) {
 			case ONE:
-				position.set(Constants.BADIO_POSITION_ONE);
+				positionMotor.set(Constants.BADIO_POSITION_ONE);
 				break;
 			case TWO:
-				position.set(Constants.BADIO_POSITION_TWO);
+				positionMotor.set(Constants.BADIO_POSITION_TWO);
 				break;
 			case THREE:
-				position.set(Constants.BADIO_POSITION_THREE);
+				positionMotor.set(Constants.BADIO_POSITION_THREE);
 				break;
 			case FOUR: 
-				position.set(Constants.BADIO_POSITION_FOUR);
+				positionMotor.set(Constants.BADIO_POSITION_FOUR);
 				break;
-			}
-		}
-		
-		public void allign(BIO bio, DIO dio) {
-			switch (position) {
-			case ONE:
-				break;
-			case TWO: 
-				
 			}
 		}
 	}
+
+	public void allign(positionOptions position) {
+		switch (position) {
+		case ONE:
+			break;
+		case TWO: 
+			break;
+		case THREE:
+			break;
+		case FOUR: 
+			break;
+		}
+	}
+
+	public void bInput(double speed) {
+		leftBIO.set(speed);
+		rightBIO.set(speed);
+	}
+	
+	public void bOutput(double speed) {
+		leftBIO.set(speed);
+		rightBIO.set(speed);
+	}
+
+	public void dInput(double speed) {}
+	public void dOutput(double speed) {}
 	
 }
--- gx-frc-19/src/org/gravitechx/frc2019/robot/subsystems/BADIOsubsystem/DIO.java
+++
@@ -1,5 +0,0 @@
-package org.gravitechx.frc2019.robot.subsystems.BADIOsubsystem;
-
-public class DIO {
-
-}
