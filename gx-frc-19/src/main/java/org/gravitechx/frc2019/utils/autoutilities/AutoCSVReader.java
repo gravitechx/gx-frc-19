7ff79e51e69c9e1cef45601b77bb6133f6d882cf
Nathan Prisbrey <ntpbanana@gmail.com>
2019-03-30 09:03:58 -0700

    Arm Implementation
    Added first draft of arm action implementation. Time waited after each action to let testers respond appropriately when testing. Removed extraneous System.out.println statements that were from previous testing.

--- gx-frc-19/src/main/java/org/gravitechx/frc2019/utils/autoutilities/AutoCSVReader.java
+++ gx-frc-19/src/main/java/org/gravitechx/frc2019/utils/autoutilities/AutoCSVReader.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import org.gravitechx.frc2019.robot.subsystems.badiosubsystem.Arm;
 
 public class AutoCSVReader {
 	private final double stallCurrentThreshold = 1.4;//NEED TO FINE TUNE WITH EXPERIMENTAL VALUES
@@ -14,24 +15,24 @@ public class AutoCSVReader {
     private boolean waitForStall = false;
     private BufferedReader br;
     private double[] latestSetpoints, nextSetpoints;
-    private double initialTime, initialStallTime;
+    private double initialTime, initialStallTime, pauseForArmMovement;
+    private Arm arm;
+    arm = Arm.getArmInstance();
+    
     public AutoCSVReader() throws FileNotFoundException, IOException{
         resetReader();
     }
+    
     public double[] getSetpoints(double leftTickSpeed, double rightTickSpeed, double[] pdpCurrents) throws IOException{
         String line;
         if(initialTime == 0) {
             initialTime = Timer.getFPGATimestamp();
         }
-        System.out.println("NextSetpoints at top: " + !Arrays.equals(nextSetpoints,new double[]{}));
         if(!Arrays.equals(nextSetpoints,new double[]{})) {//If there's a possibility that lastestSetpoints needs to be updated
-            System.out.println("Waitforstall: " + waitForStall);
             if(waitForStall) {//If we're waiting for the robot to hit a wall and stall out the motors
             	double leftPDPCurrent = (pdpCurrents[0] + pdpCurrents[1] + pdpCurrents[2])/3.0;
             	double rightPDPCurrent = (pdpCurrents[3] + pdpCurrents[4] + pdpCurrents[5])/3.0;
-                System.out.println("Against Wall: " + (Math.abs(leftTickSpeed) < stallTickSpeed && Math.abs(rightTickSpeed) < stallTickSpeed && leftPDPCurrent > stallCurrentThreshold && rightPDPCurrent > stallCurrentThreshold));
                 if(Math.abs(leftTickSpeed) < stallTickSpeed && Math.abs(rightTickSpeed) < stallTickSpeed && leftPDPCurrent > stallCurrentThreshold && rightPDPCurrent > stallCurrentThreshold) {
-                    System.out.println(Timer.getFPGATimestamp()-initialStallTime);
                     if(initialStallTime == -1) {
                         initialStallTime = Timer.getFPGATimestamp();
                     } else if(Timer.getFPGATimestamp()-initialStallTime > stallWaitTime) {
@@ -45,46 +46,61 @@ public class AutoCSVReader {
                     initialStallTime = -1;
                 }
             } else {
-	        	while(Timer.getFPGATimestamp()-initialTime >= nextSetpoints[0] && waitForStall == false) {//If it's time to start using nextSetpoints
+	        	while(Timer.getFPGATimestamp()-initialTime >= nextSetpoints[0] && waitForStall == false && Timer.getFPGATimestamp()-pauseForArmMovement >= 0) {//If it's time to start using nextSetpoints
 	                latestSetpoints = nextSetpoints;
 	                if((line = br.readLine()) != null) {//If there's any more lines to read
 	                	if(!line.substring(0,1).equals("!") && !line.substring(0,1).equals("@")) {//If line != '!!!!!UntilStall!!!!!' or '@ArmAction@'
 	                		nextSetpoints = stringsToDoubles(line.split(","));
                         } else if(line.substring(0,1).equals("!")){//If line == '!!!!!UntilStall!!!!!'
-                            System.out.println("Reading !!!!!UntilStall!!!!! loop");
 	                		waitForStall = true;
 	                		line = br.readLine();//Now line == '-'
 	                		line = br.readLine();//Now line is set to the line of setpoints to keep repeating until the motors stall
                             latestSetpoints = stringsToDoubles(line.split(","));
                             nextSetpoints = new double[]{0,0,0,0,0};
 	                		line = br.readLine();//Now line == '-'
-	                	}/*ARMACTIONS else if(line.substring(0,1).equals("@")) {
-	                	    line = br.readLine();//Now line == ChangeArmPositionTo(...) <- No quotes inside of parentheses
-                            switch(line.substring(20,line.length()-1)) {
-                                case "CargoShip" :
-                                    //Move arm to appropriate setposition
+	                	} else if(line.substring(0,1).equals("@")) {
+	                		
+                            switch(line.substring(0,21)) {
+                                case "@arm.setAutonVacuumPo" :
+                                    switch(line.substring(28,line.length()-1)) {
+                                        case "VacuumPosition.DOWN" :
+                                            arm.setAutonVacuumPosition(VacuumPosition.DOWN);
+                                        default :
+                                            System.out.println("@arm.setAutonVacuumPosition is having issues reading what is inisde the parentheses in its call in the setpoints file.");
+                                    }
+                                    pauseForArmMovement = Timer.getFPGATimestamp() + 3;
+                                case "@arm.setAutonPosition" :
+                                    switch(line.substring(22,line.length()-1)) {
+                                        case "ButtonArmPosition.CARGOBAY" :
+                                            arm.setAutonPosition(ButtonArmPosition.CARGOBAY);
+                                        default :
+                                            System.out.println("@arm.setAutonPosition is having issues reading what is inisde the parentheses in its call in the setpoints file.");
+                                    }
+                                    pauseForArmMovement = Timer.getFPGATimestamp() + 3;
+                                case "@arm.setAutonIntakeSt" :
+                                    switch(line.substring(25,line.length()-1)) {
+                                        case "IntakeState.EXHALE" :
+                                            arm.setAutonIntakeState(IntakeState.EXHALE);
+                                        case "IntakeState.NEUTRAL" :
+                                            arm.setAutonIntakeState(IntakeState.NEUTRAL);
+                                        default :
+                                            System.out.println("@arm.setAutonIntakeState is having issues reading what is inisde the parentheses in its call in the setpoints file.");
+                                    }
+                                    pauseForArmMovement = Timer.getFPGATimestamp() + 3;
                                 default :
-                                    System.out.println("AutoCSVReader didn't understand the content in the 'ChangeArmPositionTo()' setpoint.");
+                                    System.out.println("Can't read the command given in the setpoints file. The line '" + line + "' is unreadable.");
                             }
-                            line = br.readLine();//Now line == SetArmActionTo(...) <- No quotes inside of parentheses
-                            switch(line.substring(15,line.length()-1)) {
-                                case "Intake" :
-                                    //Set rollers on arm to intake
-                                case "Outtake" :
-                                    //Set rollers on arm to outtake
-                            }
-                        }*/
+                            
+                        }
 	                } else {
-                        System.out.println("RUNNING THIS THING!!! NextSetpoints Before:" + Arrays.toString(nextSetpoints));
                         nextSetpoints = new double[]{};
-                        System.out.println("NextSetpoints After:" + Arrays.toString(nextSetpoints));
 	                }
 	            }
             }
         }
-        //System.out.println("TIME: " + (Timer.getFPGATimestamp()-initialTime));
         return Arrays.copyOfRange(latestSetpoints, 1, 5);
     }
+    
     private double[] stringsToDoubles(String[] strArray) {
         double[] result = new double[strArray.length];
         for(int i = 0;i<strArray.length;i++) {
@@ -92,6 +108,7 @@ public class AutoCSVReader {
         }
         return result;
     }
+    
     public void resetReader() throws FileNotFoundException, IOException{
         br = new BufferedReader(new FileReader("/home/admin/yeetCode.txt"));
         br.readLine();
@@ -100,5 +117,6 @@ public class AutoCSVReader {
         initialTime = 0;
         waitForStall = false;
         initialStallTime = -1;
+        pauseForArmMovement = Timer.getFPGATimestamp();
     }
 }
